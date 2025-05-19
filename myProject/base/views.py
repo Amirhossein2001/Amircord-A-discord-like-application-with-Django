d0d5c06c39527e3a7a9b2d8be095fbdb3cdf6ef4
Amirhossein2001 <amirhosseinramezani4455@gmail.com>
2025-05-19 20:02:07 +0330

    Database and Models

--- myProject/base/views.py
+++ myProject/base/views.py
@@ -1,16 +1,21 @@
 from django.shortcuts import render
+from .models import Room
 
-rooms = [
+# rooms = [
     
-    {'id':1, 'name':'lets learn python'},
-    {'id':2, 'name':'lets learn JS'},
-    {'id':3, 'name':'lets learn React.js'}
-]
+#     {'id':1, 'name':'lets learn python'},
+#     {'id':2, 'name':'lets learn JS'},
+#     {'id':3, 'name':'lets learn React.js'}
+# ]
 
 
 
 def home(request):
+    rooms = Room.objects.all()
     return render(request, 'base/home.html', {'rooms': rooms})
 
 def room(request,pk):
-    return render(request, 'base/room.html')
+    room = Room.objects.get(id=pk)
+    context = {'room': room}
+   
+    return render(request, 'base/room.html', context)
